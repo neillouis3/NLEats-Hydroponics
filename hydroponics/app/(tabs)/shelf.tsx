5ba7d308507302bc016715e9b083cfef02a9ac1e
neillouis3 <neil03.castillon@gmail.com>
2023-11-20 22:14:00 -0330

    Changing layout from dark to light mode

--- hydroponics/app/(tabs)/shelf.tsx
+++ hydroponics/app/(tabs)/shelf.tsx
@@ -18,11 +18,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    
+
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    
   },
   separator: {
     marginVertical: 30,
