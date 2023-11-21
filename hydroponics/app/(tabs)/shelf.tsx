63b09214af540873c6742d51ecef2d82a12a51a5
neillouis3 <neil03.castillon@gmail.com>
2023-11-20 22:14:21 -0330

    Styling Changes

--- hydroponics/app/(tabs)/shelf.tsx
+++ hydroponics/app/(tabs)/shelf.tsx
@@ -23,11 +23,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    
+
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+    
   },
 });
