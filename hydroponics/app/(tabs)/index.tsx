e6223950599bf46d8f38acae37a6a5cdb4e58179
neillouis3 <neil03.castillon@gmail.com>
2023-11-23 22:41:13 -0330

    Styling

--- hydroponics/app/(tabs)/index.tsx
+++ hydroponics/app/(tabs)/index.tsx
@@ -3,15 +3,19 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function IndexScreen() {
+const IndexScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Index</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>NLEats</Text>
+      <Text style={styles.title}>Hydroponics</Text>
+
     </View>
   );
 }
+
+
+
+export default IndexScreen;
 
 const styles = StyleSheet.create({
   container: {
