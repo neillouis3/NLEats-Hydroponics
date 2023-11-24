a13e0c191715e6f80d34903b8f00475b1f4b0569
neillouis3 <neil03.castillon@gmail.com>
2023-11-23 22:32:29 -0330

    Changing layout

--- hydroponics/app/(tabs)/_layout.tsx
+++ hydroponics/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs, Stack } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
+
 
 
 import Colors from '../../constants/Colors';
@@ -16,42 +16,3 @@ function TabBarIcon(props: {
 }
 
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
