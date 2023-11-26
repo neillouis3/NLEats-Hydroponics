ce25ecb52b410a9939bbfac8bba32efe02f52c02
neillouis3 <neil03.castillon@gmail.com>
2023-11-25 23:38:36 -0330

    Added hyperlink component

---
+++ hydroponics/components/HyperLink.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default class HyperLink extends React.Component {
+    return (
+        
+    )
+}
