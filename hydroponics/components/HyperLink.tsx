8f2848e68255597266e711190a9b1ff8b8cc20e5
neillouis3 <neil03.castillon@gmail.com>
2023-11-25 23:40:38 -0330

    Fixed error in hyperlink component

--- hydroponics/components/HyperLink.tsx
+++ hydroponics/components/HyperLink.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 
-export default class HyperLink extends React.Component {
+export default function HyperLink () {
     return (
-        
-    )
+        <View style={styles.container}>
+            <Text style={styles.text}>HyperLink</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    text: {
+
+    },
+});
