a2655641b53f841e9fdc2aa33eb86afa97cc2b49
neillouis3 <neil03.castillon@gmail.com>
2023-11-26 23:41:26 -0330

    Styling

--- hydroponics/app/(tabs)/login.js
+++ hydroponics/app/(tabs)/login.js
@@ -50,7 +50,14 @@ const styles = StyleSheet.create({
 
   },
 
-
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: '75%',
+  },
+  
   image:{
     resizeMode: 'stretch',
     width: '75%',
