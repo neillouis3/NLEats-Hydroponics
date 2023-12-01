a99bf9bba1f845f87b30018f6b1743ab50145ea5
neillouis3 <neil03.castillon@gmail.com>
2023-11-30 20:35:47 -0330

    Changes on entry screen

--- hydroponics/app/(entry)/index.js
+++ hydroponics/app/(entry)/index.js
@@ -28,7 +28,6 @@ const IndexScreen = () => {
       </Link>
 
 
-
     </ScrollView>
   );
 }
---
+++ hydroponics/app/_layout.tsx
@@ -0,0 +1,19 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
+
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </ThemeProvider>
+  );
+}
